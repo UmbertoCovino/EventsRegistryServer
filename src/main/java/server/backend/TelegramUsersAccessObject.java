09794325eb1d6c883c685d0bc4d4056535a20bf9
AngeloCaporaso <56113767+AngeloCaporaso@users.noreply.github.com>
2020-01-05 16:32:19 +0100

    modified storing mode chat_id

--- src/main/java/server/backend/TelegramUsersAccessObject.java
+++ src/main/java/server/backend/TelegramUsersAccessObject.java
@@ -1,29 +1,84 @@
 package server.backend;
 
+import java.sql.ResultSet;
+import java.sql.SQLException;
 import java.util.TreeMap;
 
+import commons.exceptions.GenericSQLException;
 import commons.exceptions.InvalidUserEmailException;
 import commons.exceptions.InvalidUserTokenException;
+import commons.exceptions.VoidClassFieldException;
 import commons.User;
 
-public class TelegramUsersRegistry {
+public class TelegramUsersAccessObject {
 	private TreeMap<Integer, User> token_map;
 	private TreeMap<String, Long> chat_id_map;	// key: user_email, object: chat_id
 	private int token;
-	private static TelegramUsersRegistry instance;
-
-	private TelegramUsersRegistry() {
+	private static TelegramUsersAccessObject instance;
+	
+	private TelegramUsersAccessObject() {
 		token_map = new TreeMap<Integer, User>();
 		chat_id_map = new TreeMap<String, Long>();
 		token = 0; //initialize with file_log
 	}
 	
-	public static synchronized TelegramUsersRegistry instance() {
+	public static synchronized TelegramUsersAccessObject instance() {
 		if (instance == null)
-			instance = new TelegramUsersRegistry();
+			instance = new TelegramUsersAccessObject();
 		
 		return instance;
 	}
+	
+	public synchronized static long getUserChatId(String email) throws InvalidUserEmailException, GenericSQLException, VoidClassFieldException {
+		long chat_id;
+
+		if (email == null || email.equals(""))
+			throw new VoidClassFieldException("The email passed cannot be null or empty.");
+		
+		try {
+			ResultSet rs = DBManager.executeQuery("select chat_id from users where email = '" + email + "';");
+
+			if (rs.next()) {
+				chat_id = rs.getLong("chat_id");
+			} else
+				throw new InvalidUserEmailException("Inexistent user email: " + email);
+			
+			rs.close();
+		} catch (SQLException e) {
+			throw new GenericSQLException(e.getMessage());
+		}
+		
+		return chat_id;
+	}
+	
+	public synchronized static int updateUserChatId(String email, long chat_id) throws InvalidUserEmailException, GenericSQLException, VoidClassFieldException {
+		int result = 0;
+		
+		if (email == null || email.equals(""))
+			throw new VoidClassFieldException("The email passed cannot be null or empty.");
+		
+		try {
+			ResultSet rs = DBManager.executeQuery("select count(*) as users_number from users where email = '" + email + "';");
+			
+			if (rs.next()) {
+				if (rs.getInt("users_number") == 0)
+					throw new InvalidUserEmailException("Inexistent user email: " + email);
+				
+				result = DBManager.executeUpdate("update users "
+											   + "set chat_id = '" + chat_id + "' "
+											   + "where email = '" + email + "';");
+			}
+		} catch (SQLException e) {
+			throw new GenericSQLException(e.getMessage());
+		}
+		
+		if (result == 1)
+			return 1;
+		else
+			throw new GenericSQLException("An error occurred while updating user to DB.");
+	}
+	
+	///////////////////////////////////////////// tokens_map management /////////////////////////////////////////////////
 	
 	public synchronized int getNextAvailableToken() {
 		return ++token;

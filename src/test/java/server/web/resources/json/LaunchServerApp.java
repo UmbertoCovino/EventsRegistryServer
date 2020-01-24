d897be1fde62fdd8b54760ab98c5cb348db64483
umberto <umberto@192.168.1.23>
2020-01-25 00:04:50 +0100

    Bug fix.

--- src/test/java/server/web/resources/json/LaunchServerApp.java
+++ src/test/java/server/web/resources/json/LaunchServerApp.java
@@ -3,13 +3,12 @@ package server.web.resources.json;
 import server.web.frontend.EventsRegistryWebApplication;
 
 public class LaunchServerApp {
-	
 	private static boolean alreadyRunned = false;
 	
 	public synchronized static void execute() {
-		if(!alreadyRunned) {
+		if (!alreadyRunned) {
 			EventsRegistryWebApplication.main(null);
+			alreadyRunned = true;
 		}
 	}
-
 }

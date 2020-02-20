956d3b86cc3bf0bf567c146b41b34f63193a92e1
UmbertoCovino <upcovino@gmail.com>
2020-02-20 19:02:58 +0100

    Updated pom with database integration.

--- events_registry.sql
+++ events_registry.sql
@@ -1,10 +1,10 @@
-drop database if exists events_registry;
+--drop database if exists events_registry;
 
-create database events_registry;
+create database if not exists events_registry;
 
 use events_registry;
 
-create table users (
+create table if not exists users (
 	name varchar(80) not null,
 	surname varchar(80) not null,
 	email varchar(80) primary key,
@@ -13,7 +13,7 @@ create table users (
 	chat_id bigint(20) unsigned 
 );
 
-create table events (
+create table if not exists events (
 	id int(8) primary key auto_increment,
 	title varchar(80) not null,
 	start_date datetime not null,
@@ -24,7 +24,7 @@ create table events (
 	foreign key (user_owner_email) references users(email)
 );
 
-create table events_users_participations (
+create table if not exists events_users_participations (
 	event_id int(8) not null,
 	user_email varchar(80) not null,
 	primary key (event_id, user_email),
